e0d357c47d24d5ef4a096a5e9467280ca3639544
Luis Rishi Puno <LRPuno@gmail.com>
2018-08-09 02:53:30 -0400

    MVP 3

--- state3.js
+++ state3.js
@@ -15,22 +15,14 @@ brawl.state3.prototype= {
 
         //	Font style
         text.font = 'Arial Black';
-        text.fontSize = 50;
+        text.fontSize = 20;
         text.fill= "#19de65";
         text.fontWeight = 'bold';
-
-        //	Stroke color and thickness
-        text.stroke = '#000000';
-        text.strokeThickness = 8;
-        text.fill = '#43d637';
 
         //  And now we'll color in some of the letters
         text.addColor('#ff00ff', 9);
         text.addColor('#43d637', 13);
 
-        //  This allows us to color the stroke instead of the letters
-        text.addStrokeColor('#ff0000', 13);
-        text.addStrokeColor('#000000', 20);
 
 
 
